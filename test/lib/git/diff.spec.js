c77392e6ea8b2ccc96b71fb9c8a0e7298cb92a56
Tim Paulaskas <Tim.Paulaskas@docusign.com>
2023-01-19 19:58:28 -0800

    refactor: removed extra layers from tests

--- test/lib/git/diff.spec.js
+++ test/lib/git/diff.spec.js
@@ -10,45 +10,43 @@ jest.mock('child_process', () => ({
 }))
 const gitRef = "HEAD~1..HEAD"
 
-describe('diff', () => {
-    beforeEach(() => {
-        jest.clearAllMocks()
-    })
+beforeEach(() => {
+    jest.clearAllMocks()
+})
 
-    test('rejects if directory is not a git repository', async () => {
-        existsSync.mockReturnValueOnce(false)
-        try {
-            await diff('/path/to/dir')
-            fail('Expected function to throw an error', gitRef, existsSync)
-        } catch (error) {
-            expect(error.message).toEqual('The directory "/path/to/dir" is not a git repository')
-        }
-    })
+test('rejects if directory is not a git repository', async () => {
+    existsSync.mockReturnValueOnce(false)
+    try {
+        await diff('/path/to/dir')
+        fail('Expected function to throw an error', gitRef, existsSync)
+    } catch (error) {
+        expect(error.message).toEqual('The directory "/path/to/dir" is not a git repository')
+    }
+})
 
-    test('resolves with files when git diff command is successful', async () => {
-        existsSync.mockReturnValueOnce(true).mockReturnValueOnce(true)
-        execSync.mockReturnValueOnce(
-`A\tfile1.txt
+test('resolves with files when git diff command is successful', async () => {
+    existsSync.mockReturnValueOnce(true).mockReturnValueOnce(true)
+    execSync.mockReturnValueOnce(
+        `A\tfile1.txt
 M\tfile2.txt
 D\tfile3.txt`)
 
-        const files = await diff('/path/to/dir', gitRef, existsSync, execSync)
-        expect(files).toEqual([
-            { type: 'add', path: 'file1.txt', action: 'add' },
-            { type: 'modify', path: 'file2.txt', action: 'add' },
-            { type: 'delete', path: 'file3.txt', action: 'delete' }
-        ])
-    })
+    const files = await diff('/path/to/dir', gitRef, existsSync, execSync)
+    expect(files).toEqual([
+        { type: 'add', path: 'file1.txt', action: 'add' },
+        { type: 'modify', path: 'file2.txt', action: 'add' },
+        { type: 'delete', path: 'file3.txt', action: 'delete' }
+    ])
+})
 
-    test('rejects when git diff command fails', async () => {
-        existsSync.mockReturnValueOnce(true).mockReturnValueOnce(true)
-        execSync.mockImplementation(() => { throw new Error('Command failed') })
+test('rejects when git diff command fails', async () => {
+    existsSync.mockReturnValueOnce(true).mockReturnValueOnce(true)
+    execSync.mockImplementation(() => { throw new Error('Command failed') })
 
-        try {
-            await diff('/path/to/dir', gitRef, existsSync, execSync)
-            fail('Expected function to throw an error')
-        } catch (error) {
-            expect(error.message).toEqual('Command failed')
-        }
-    })
+    try {
+        await diff('/path/to/dir', gitRef, existsSync, execSync)
+        fail('Expected function to throw an error')
+    } catch (error) {
+        expect(error.message).toEqual('Command failed')
+    }
 })
--- test/lib/git/log.spec.js
+++ test/lib/git/log.spec.js
@@ -8,26 +8,24 @@ jest.mock('child_process', () => {
     }
 })
 
-describe('log', () => {
-    beforeEach(() => {
-        jest.resetAllMocks()
-    })
+beforeEach(() => {
+    jest.resetAllMocks()
+})
 
-    it('should return an array of git commit hashes', () => {
-        const commits = ['1234567890abcdef', '234567890abcdef1', '34567890abcdef12']
-        execSync.mockReturnValue(commits.join(os.EOL))
-        const dir = process.cwd()
-        const gitRef = 'HEAD~1..HEAD'
-        const result = log(dir, gitRef, execSync)
-        expect(execSync).toHaveBeenCalledWith(`git log --format=format:%H ${gitRef}`, { cwd: dir, encoding: 'utf-8' })
-        expect(result).toEqual(commits)
-    })
+it('should return an array of git commit hashes', () => {
+    const commits = ['1234567890abcdef', '234567890abcdef1', '34567890abcdef12']
+    execSync.mockReturnValue(commits.join(os.EOL))
+    const dir = process.cwd()
+    const gitRef = 'HEAD~1..HEAD'
+    const result = log(dir, gitRef, execSync)
+    expect(execSync).toHaveBeenCalledWith(`git log --format=format:%H ${gitRef}`, { cwd: dir, encoding: 'utf-8' })
+    expect(result).toEqual(commits)
+})
 
-    it('should throw an error if git is not installed or no entry found in path', () => {
-        const error = { message: 'ENOENT' }
-        execSync.mockImplementation(() => { throw error })
-        const dir = process.cwd()
-        const gitRef = 'HEAD~1..HEAD'
-        expect(() => log(dir, gitRef, execSync)).toThrowError('git not installed or no entry found in path')
-    })
+it('should throw an error if git is not installed or no entry found in path', () => {
+    const error = { message: 'ENOENT' }
+    execSync.mockImplementation(() => { throw error })
+    const dir = process.cwd()
+    const gitRef = 'HEAD~1..HEAD'
+    expect(() => log(dir, gitRef, execSync)).toThrowError('git not installed or no entry found in path')
 })
